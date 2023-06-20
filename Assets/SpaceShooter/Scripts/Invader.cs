3833848f926f478fb44e65dcfa4985e0eeee3f76
Toxic Boy <TocHack143@gmail.com>
2023-06-20 23:49:35 +0530

    Scripts Change

---
+++ Assets/SpaceShooter/Scripts/Invader.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+namespace SpaceShooter
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Invader : MonoBehaviour
+    {
+        public SpriteRenderer spriteRenderer { get; private set; }
+        public Sprite[] animationSprites = new Sprite[0];
+        public float animationTime = 1f;
+        public int animationFrame { get; private set; }
+        public int score = 10;
+        public System.Action<Invader> killed;
+
+        private void Awake()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.sprite = animationSprites[0];
+        }
+
+        private void Start()
+        {
+            InvokeRepeating(nameof(AnimateSprite), animationTime, animationTime);
+        }
+
+        private void AnimateSprite()
+        {
+            animationFrame++;
+
+            // Loop back to the start if the animation frame exceeds the length
+            if (animationFrame >= animationSprites.Length)
+            {
+                animationFrame = 0;
+            }
+
+            spriteRenderer.sprite = animationSprites[animationFrame];
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.layer == LayerMask.NameToLayer("Laser"))
+            {
+                killed?.Invoke(this);
+            }
+        }
+
+    }
+}
