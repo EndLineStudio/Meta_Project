59a282dc5866dabf99ef035355ae5a61b2173d91
Toxic Boy <TocHack143@gmail.com>
2023-06-21 20:34:45 +0530

    SpaceShooter Update

---
+++ Assets/SpaceShooter/Scripts/GameManager.cs
@@ -0,0 +1,125 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SpaceShooter
+{
+    public sealed class GameManager : MonoBehaviour
+    {
+        private Player player;
+        private Invaders invaders;
+        private MysteryShip mysteryShip;
+        private Bunker[] bunkers;
+
+        public GameObject gameOverUI;
+        public Text scoreText;
+        public Text livesText;
+
+        public int score { get; private set; }
+        public int lives { get; private set; }
+
+        private void Awake()
+        {
+            player = FindObjectOfType<Player>();
+            invaders = FindObjectOfType<Invaders>();
+            mysteryShip = FindObjectOfType<MysteryShip>();
+            bunkers = FindObjectsOfType<Bunker>();
+        }
+
+        private void Start()
+        {
+            player.killed += OnPlayerKilled;
+            mysteryShip.killed += OnMysteryShipKilled;
+            invaders.killed += OnInvaderKilled;
+
+            NewGame();
+        }
+
+        private void Update()
+        {
+            if (lives <= 0 && Input.GetKeyDown(KeyCode.Return))
+            {
+                NewGame();
+            }
+        }
+
+        private void NewGame()
+        {
+            gameOverUI.SetActive(false);
+
+            SetScore(0);
+            SetLives(3);
+            NewRound();
+        }
+
+        private void NewRound()
+        {
+            invaders.ResetInvaders();
+            invaders.gameObject.SetActive(true);
+
+            for (int i = 0; i < bunkers.Length; i++)
+            {
+                bunkers[i].ResetBunker();
+            }
+
+            Respawn();
+        }
+
+        private void Respawn()
+        {
+            Vector3 position = player.transform.position;
+            position.x = 0f;
+            player.transform.position = position;
+            player.gameObject.SetActive(true);
+        }
+
+        private void GameOver()
+        {
+            gameOverUI.SetActive(true);
+            invaders.gameObject.SetActive(false);
+        }
+
+        private void SetScore(int score)
+        {
+            this.score = score;
+            scoreText.text = score.ToString().PadLeft(4, '0');
+        }
+
+        private void SetLives(int lives)
+        {
+            this.lives = Mathf.Max(lives, 0);
+            livesText.text = lives.ToString();
+        }
+
+        private void OnPlayerKilled()
+        {
+            SetLives(lives - 1);
+
+            player.gameObject.SetActive(false);
+
+            if (lives > 0)
+            {
+                Invoke(nameof(NewRound), 1f);
+            }
+            else
+            {
+                GameOver();
+            }
+        }
+
+        private void OnInvaderKilled(Invader invader)
+        {
+            SetScore(score + invader.score);
+
+            if (invaders.AmountKilled == invaders.TotalAmount)
+            {
+                NewRound();
+            }
+        }
+
+        private void OnMysteryShipKilled(MysteryShip mysteryShip)
+        {
+            SetScore(score + mysteryShip.score);
+        }
+
+    }
+}
---
+++ Assets/SpaceShooter/Scripts/Player.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+
+namespace SpaceShooter
+{
+    public class Player : MonoBehaviour
+    {
+        public float speed = 5f;
+        public Projectile laserPrefab;
+        public System.Action killed;
+        public bool laserActive { get; private set; }
+
+        private void Update()
+        {
+            Vector3 position = transform.position;
+
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                position.x -= speed * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                position.x += speed * Time.deltaTime;
+            }
+
+            Vector3 leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+            Vector3 rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
+
+            // Clamp the position of the character so they do not go out of bounds
+            position.x = Mathf.Clamp(position.x, leftEdge.x, rightEdge.x);
+            transform.position = position;
+
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+            {
+                Shoot();
+            }
+        }
+
+        private void Shoot()
+        {
+            // Only one laser can be active at a given time so first check that
+            // there is not already an active laser
+            if (!laserActive)
+            {
+                laserActive = true;
+
+                Projectile laser = Instantiate(laserPrefab, transform.position, Quaternion.identity);
+                laser.destroyed += OnLaserDestroyed;
+            }
+        }
+
+        private void OnLaserDestroyed(Projectile laser)
+        {
+            laserActive = false;
+        }
+
+        /// <summary>
+        /// Player Killed Logic.
+        /// </summary>
+        /// <param name="other"></param>
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.layer == LayerMask.NameToLayer("Missile") ||
+                other.gameObject.layer == LayerMask.NameToLayer("Invader"))
+            {
+                if (killed != null)
+                {
+                    killed.Invoke();
+                }
+            }
+        }
+
+    }
+}
